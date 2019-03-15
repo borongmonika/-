abae6591e25618e7d1e60764d9224cf522508518
borongmonika <15100377989@163.com>
2019-03-15 17:27:35 +0800

    分类组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,45 +1,61 @@
 import React, { Component } from 'react';
-import { maxHeaderSize } from 'http';
+import TodoItem from './TodoItem';
 
 class TodoList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      list:[
-        'gogo',
-        'gogo1'
-          ],
-      inputValue:''
+      list: [
+      ],
+      inputValue: ''
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleDelete = this.handleDelete.bind(this)
   }
-  handleBtnClick(){
+  handleBtnClick() {
     this.setState({
-      list: [...this.state.list,this.state.inputValue],
-      inputValue:''
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
     })
   }
-  handleInputChange(e){
+  handleInputChange(e) {
     this.setState({
-      inputValue:e.target.value
+      inputValue: e.target.value
     })
   }
-  handleItemClick(index){
-    const list = [...this.state.list]
-    list.splice(index,1);
-    this.setState({list})
 
+  handleDelete(index) {
+    const list = [...this.state.list]
+    list.splice(index, 1);
+    this.setState({ list })
   }
+  getTodoItems() {
+    return (
+      this.state.list.map((item, index) => {
+        return (
+          <TodoItem
+            delete={this.handleDelete.bind(this)}
+            key={index}
+            content={item}
+            index={index}
+          />
+          //return <li onClick={this.handleItemClick.bind(this, index)} key = {index}>{item}</li>
+        )
+      })
+    )
+  }
+  //父组件通过属性的形式向子组件传递参数
+  //子组件通过props接受父组件传递过来的参数
   render() {
     return (
       <div>
         <div>
-          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
-          <button onClick={this.handleBtnClick.bind(this)}>add</button>
+          <input value={this.state.inputValue} onChange={this.handleInputChange} />
+          <button  onClick={this.handleBtnClick}>add</button>
         </div>
         <ul>
-          {this.state.list.map((item,index)=>{
-            return <li onClick={this.handleItemClick.bind(this, index)} key = {index}>{item}</li>
-          })}
+        {this.getTodoItems()}
         </ul>
       </div>
     )
