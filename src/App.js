e4daa600213f1263a1ff1e96d7073f2b22b0de71
borongmonika <15100377989@163.com>
2019-03-18 15:58:34 +0800

    补全App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React,{Component} from 'react';
 
 
 
@@ -26,7 +27,6 @@ class Go extends Component {
         return (
             <div>
                 <h2>{this.props.name}:rua!</h2>
-                <Button></Button>
                 <ul>
                     {this.state.solders.map((num,index)=>{
                         return <li key={index}>{num}</li>
