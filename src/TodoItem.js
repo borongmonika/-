c8a92e720e8332f8c2863daf27cdb67a8ade2dab
borongmonika <15100377989@163.com>
2019-03-28 17:46:28 +0800

    3/28

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,25 +1,14 @@
-import React from 'react';
-class TodoItem extends React.Component{
-    //子组件如果想和父组件通信，子组件要调用父组件传递过来的方法
-    constructor(props){
-        super(props);
-        this.handleDelete = this.handleDelete.bind(this)
-
+import React, { Component } from 'react';
+class TodoItem extends Component {
+    handelClick(){
+        alert(1)
     }
-    handleDelete(){
-        // const { delete, index } = this.props;
-        // delete(index)
-        this.props.delete(
-            this.props.index
-        );
-   
-
-
-    }
-    render(){
-        const{ content } = this.props;
-        return(
-            <li onClick={this.handleDelete}>{content}</li>
+    render() {
+       
+        return (
+            <div onClick={this.handelClick}>
+                {this.props.content}
+            </div>
         )
     }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,62 @@
-import React ,{ Component,Fragment } from "react";
-
-class TodoList extends Component{
-  constructor(props){
+import React, { Component, Fragment } from "react";
+import TodoItem from './TodoItem';
+class TodoList extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      inputValue: '111',
+      inputValue: '',
       list: []
     }
   }
   handelInputChange(e) {
-    console.log(e.target.value)
+    this.setState({
+      inputValue: e.target.value
+    })
   }
-  render(){
-  
-    return(
+  handelBtnClick() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+    console.log('添加成功')
+  }
+  handelItemDelete(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({
+      list: list
+    })
+    console.log('删除成功')
+
+  }
+  render() {
+
+    return (
       <Fragment>
+      <div>
+        <label htmlFor='insertArea'>输入：</label>
         <input
+          id='insertArea'
           value={this.state.inputValue}
-          onChange={this.handelInputChange}
+          onChange={this.handelInputChange.bind(this)}
         />
-        <button>RUA!</button>
+        <button onClick={this.handelBtnClick.bind(this)}>RUA!</button>
+      </div>
         <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <div>
+                <TodoItem content={item}/>
+                {/* <li key={index}
+                onClick={this.handelItemDelete.bind(this, index)}
+                dangerouslySetInnerHTML={{__html: item}}
+                >
+                </li> */}
+                </div>
+              )
+            })
+          }
         </ul>
       </Fragment>
     )
