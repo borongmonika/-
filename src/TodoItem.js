b7ce9bfdb2019d5c98f91fe86bd71eb75ad97391
borongmonika <15100377989@163.com>
2019-03-29 14:42:05 +0800

    ToList

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 class TodoItem extends Component {
-    handelClick(){
-        alert(1)
-    }
-    render() {
-       
-        return (
-            <div onClick={this.handelClick}>
-                {this.props.content}
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.handelClick = this.handelClick.bind(this);
+	}
+	handelClick() {
+		const { deleteItem,index } = this.props;
+		deleteItem(index);
+		// this.props.deleteItem(this.props.index);
+		alert(this.props.content)
+	}
+	render() {
+		const { content } = this.props;
+		return (
+			<li
+				onClick={this.handelClick}>
+				{content}
+			</li>
+		)
+	}
 }
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -7,56 +7,83 @@ class TodoList extends Component {
       inputValue: '',
       list: []
     }
+    this.handelInputChange = this.handelInputChange.bind(this)
+    this.handelBtnClick = this.handelBtnClick.bind(this)
+    this.handelItemDelete = this.handelItemDelete.bind(this)
   }
   handelInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
+    const value = e.target.value;
+    this.setState(() =>({
+        inputValue: value
+      }))
+    // this.setState({
+    //   inputValue: e.target.value
+    // })
   }
   handelBtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
+    //prevState = this.state
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
       inputValue: ''
-    })
-    console.log('添加成功')
+    }))
+    // this.setState(() => ({
+    //   list: [...this.state.list, this.state.inputValue],
+    //   inputValue: ''
+    // }))
+    // this.setState({
+    //   list: [...this.state.list, this.state.inputValue],
+    //   inputValue: ''
+    // })
+    console.log('添加成功:' + this.state.inputValue)
   }
   handelItemDelete(index) {
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({
-      list: list
+    // const list = [...this.state.list];
+    // list.splice(index, 1);
+    // this.setState({
+    //   list: list
+    // })
+    this.setState((prevState) =>{
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return {list}
     })
     console.log('删除成功')
-
+  }
+  getTodoItem(){
+    return this.state.list.map((item, index) => {
+      return (
+        <div key={index}>
+        {/* key值写在最外层 */}
+          <TodoItem 
+            // key={index}
+            content={item}
+            index={index}
+            deleteItem={this.handelItemDelete}
+          />
+          {/* <li key={index}
+        onClick={this.handelItemDelete.bind(this, index)}
+        dangerouslySetInnerHTML={{__html: item}}
+        >
+        </li> */}
+        </div>
+      )
+    })
   }
   render() {
 
     return (
       <Fragment>
-      <div>
-        <label htmlFor='insertArea'>输入：</label>
-        <input
-          id='insertArea'
-          value={this.state.inputValue}
-          onChange={this.handelInputChange.bind(this)}
-        />
-        <button onClick={this.handelBtnClick.bind(this)}>RUA!</button>
-      </div>
+        <div>
+          <label htmlFor='insertArea'>输入：</label>
+          <input
+            id='insertArea'
+            value={this.state.inputValue}
+            onChange={this.handelInputChange}
+          />
+          <button onClick={this.handelBtnClick}>RUA!</button>
+        </div>
         <ul>
-          {
-            this.state.list.map((item, index) => {
-              return (
-                <div>
-                <TodoItem content={item}/>
-                {/* <li key={index}
-                onClick={this.handelItemDelete.bind(this, index)}
-                dangerouslySetInnerHTML={{__html: item}}
-                >
-                </li> */}
-                </div>
-              )
-            })
-          }
+         {this.getTodoItem()}
         </ul>
       </Fragment>
     )
