441fb73fa89e3e91b4a4507e8308197c617ab034
borongmonika <15100377989@163.com>
2019-04-09 16:41:31 +0800

    anttodolist

--- src/AntTodoList.js
+++ src/AntTodoList.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import store from './store/index';
 
 class AntTodolist extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        console.log( this.state);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange)
+    }
+    handleStoreChange() {
+        this.setState(store.getState());
+    }
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+        console.log(e.target.value);
+    }
+    handleBtnClick() {
+        const action = {
+            type: 'add_todo_item'
+        };
+        store.dispatch(action);
+    }
     render() {
         return (<div style={{ marginTop: 10 }}>
-            <Input placeholder='jojo' style={{ width: 300, marginRight: 10 }} />
-            <Button type="primary">Rua!</Button>
+            <Input 
+                value={this.state.inputValue} 
+                placeholder='jojo' 
+                style={{ width: 300, marginRight: 10 }}
+                onChange={this.handleInputChange} />
+            <Button type="primary"
+                onClick={this.handleBtnClick}>Rua!</Button>
             <List
                 size="small"
                 style={{marginTop:10, width:300}}
                 bordered
-                dataSource={data}
+                dataSource={this.state.list}
                 renderItem={item => (<List.Item>{item}</List.Item>)}
             />
         </div>)
